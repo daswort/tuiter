c93daa3d7e0897cdc7358947bb2c9aeafec567c1
Jacob Vidal <firstcommand@gmail.com>
2012-05-20 20:36:02 -0400

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuiter::Application.config.secret_token = '5b38cd2915226bf7df5c2b6cc83ae7363edfead294f43c926a0d3d0fa0230a9b651d78bbad12ff615b3482408e5667b98aeed623c070b207615cfc9cba312d45'
