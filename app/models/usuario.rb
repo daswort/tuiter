d01dec7dd36c788a69dc27af165e0d1f205fc580
Jacob Vidal <firstcommand@gmail.com>
2012-05-21 00:11:47 -0400

    Terminando modelo de Usuario (con contrasena segura)

---
+++ app/models/usuario.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: usuarios
+#
+#  id         :integer         not null, primary key
+#  nombre     :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class Usuario < ActiveRecord::Base
+  attr_accessible :nombre, :email, :password, :password_confirmation
+  has_secure_password
+  
+  before_save { |usuario| usuario.email = email.downcase }
+  
+  validates :nombre,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+end
