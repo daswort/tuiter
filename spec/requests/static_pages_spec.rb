f95b459f72afa9d78cef1a36fe65e609361bb021
Jacob Vidal <firstcommand@gmail.com>
2014-07-26 12:30:56 -0400

    terminando paginas estaticas

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Tuiter'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Tuiter')
+    end
+
+    it "should have the title 'Inicio'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Tuiter | Inicio")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Ayuda'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Ayuda')
+    end
+
+    it "should have the title 'Ayuda'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Tuiter | Ayuda")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'Nosotros'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('Nosotros')
+    end
+
+    it "should have the title 'Nosotros'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Tuiter | Nosotros")
+    end
+  end
+end
